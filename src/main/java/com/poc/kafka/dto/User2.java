e569e69f3ccfe083df5d668417d69bf4f58c4808
edgarrth <edgarrt@gmail.com>
2025-11-26 22:52:57 -0500

    Add files via upload

---
+++ src/main/java/com/poc/kafka/dto/User2.java
@@ -0,0 +1,13 @@
+package com.poc.kafka.dto;
+
+import lombok.Data;
+
+@Data
+public class User2 {
+
+    String ideUser;
+    String nombres;
+    int edad;
+    String rol;
+
+}
---
+++ src/main/java/com/poc/kafka/dto/User3.java
@@ -0,0 +1,13 @@
+package com.poc.kafka.dto;
+
+import lombok.Data;
+
+@Data
+public class User3 {
+
+    String idUser;
+    String nombres;
+    int edad;
+    String rol;
+
+}
